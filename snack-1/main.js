b8ceb5dd75bb7968a577e1fff9443ac8499e831f
antonella1986 <antonella.leo.mail@gmail.com>
2025-02-13 10:27:57 +0100

    rename folder

---
+++ snack-1/main.js
@@ -0,0 +1,8 @@
+/* L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore. */
+
+const userChoise = prompt ("Inserisci un numero");
+console.log(userChoise);
+
+const userChoisee = prompt ("Inserisci un altro numero");
+console.log(userChoisee);
