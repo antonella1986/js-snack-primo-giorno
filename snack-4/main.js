dbe59c9ecbe4033f9b86cb248ff41c4830aaf70a
antonella1986 <antonella.leo.mail@gmail.com>
2025-02-13 13:02:00 +0100

    correction

--- snack-4/main.js
+++ snack-4/main.js
@@ -7,10 +7,13 @@ se è dispari inseriscilo nell’array. */
 let emptyArray = [];
 
 for (let i = 0; i < 6; i++) {
-    const userNumber = Number(prompt("Inserisci un numero"))
+    const userNumber = Number(prompt("Inserisci un numero"));
+    console.log(userNumber);
+    
     if (userNumber % 2 !== 0) {
         console.log(userNumber);
+        oddNumbers.push(userNumber);
     }
 }
 
-console.log();
+console.log(oddNumbers);
