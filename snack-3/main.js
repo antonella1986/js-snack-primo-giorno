0bdcbfe830417c45f51e9db6e491245e476d4df8
antonella1986 <antonella.leo.mail@gmail.com>
2025-02-13 12:02:30 +0100

    snack 3

---
+++ snack-3/main.js
@@ -0,0 +1,11 @@
+/* Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. */
+
+
+for (let i = 0; i <= 10; i++) {
+    let Number = prompt("Inserisci un numero");
+    sum = Number
+}
+
+console.log();
+
