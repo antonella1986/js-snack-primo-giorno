6d7612d1acd5abcb4b35e6d638ca598a4bce31ab
antonella1986 <antonella.leo.mail@gmail.com>
2025-02-13 12:28:24 +0100

    correction

--- snack-3/main.js
+++ snack-3/main.js
@@ -2,10 +2,19 @@
 Il programma stampa la somma di tutti i numeri inseriti. */
 
 
-for (let i = 0; i <= 10; i++) {
+/* for (let i = 0; i <= 10; i++) {
     let userChoise = Number(prompt("Inserisci un numero"));
 
 }
 
-console.log();
+console.log(); */
 
+let sum = 0;
+
+for (let i = 0; i < 10; i++) {
+    const userNumber = Number(prompt("Inserisci un numero"))
+    console.log(userNumber);
+    sum = sum + userNumber;
+}
+
+console.log(sum);
